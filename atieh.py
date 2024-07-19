898d5fe0d6adba0aa587afc82214389b3c3ec698
Atieh <ati.ra341@gmail.com>
2024-07-20 00:23:17 +0330

    change names

--- atieh.py
+++ atieh.py
@@ -6,21 +6,20 @@ import re
 import concurrent.futures
 
 phone_names = [
-    "Samsung - Galaxy A15 5G 128GB (Unlocked) - Blue Black",
-    "Apple iPhone 14 Pro Max 256GB Fully Unlocked Purple - Grade B",
+    "Samsung Galaxy A15 5G Dual SIM, 4GB RAM, 128GB Storage, Blue Black (UAE Version)",
+    "Apple iPhone 14 Pro Max, 6.7 Super Retina XDR Display, A16 Bionic Chip, 6GB RAM, 256GB Storage, Physical Sim & e-Sim, 5G Network, Face ID, 48+12+12 MP Camera, Japan Version, Purple | JP14MX256P",
     "Samsung Galaxy A55",
-    "Samsung Galaxy S24 Ultra STANDARD EDITION DUAL SIM 512GB ROM + 12GB RAM (GSM | CDMA) Factory Unlocked 5G Smartphone (Titanium Black)",
+    "Samsung Galaxy S24 Ultra, 512GB, Black, 12GB RAM, Android Smartphone, 200MP Camera, (International Version)",
     "Motorola Moto G Play 2023 3GB/32GB 16MP Camera",
-    "OnePlus 12 12GB RAM+256GB Dual-SIM Unlocked Android Smartphone Supports Fastest 50W Wireless Charging",
-    "Samsung Galaxy A34 EE DUAL SIM 128GB ROM + 6GB RAM (GSM Only | No CDMA) Factory Unlocked 5G Smartphone",
-    "Xiaomi 14 Ultra DUAL SIM 512GB ROM + 16GB RAM (GSM | CDMA) Factory Unlocked 5G Smartphone (White)",
-    "Samsung - Galaxy A15 5G 128GB (Unlocked) - Blue Black",
-    "Apple iPhone 15 Pro Max, 6.7 OLED Super Retina XDR Display, A17 Pro Chip, 6-Core CPU, 6-Core GPU, 256GB Storage, 5G / Wi-Fi 6E / Bluetooth 5.3, Face ID, TRA Version, Natural Titanium | S300856271",
-    "Motorola Moto G Play 2023 3GB/32GB 16MP Camera - Navy Blue",
-    "HUAWEI nova 10 Dual Sim Smartphone, 6.67 Curved OLED Display, 8GB RAM 256 GB Storage, LTE 4G, 60 MP Front Ultra Wide Camera, "
-    "HUAWEI nova 11i SmartPhone, 6.8 FullView Display, 94.9 Screen-to-Body Ratio, 8GB+128GB, 40W SuperCharge, 5000 mAh Battery"
-    "vivo V30 5G (Noble Black, 12GB+12GB RAM, 512GB)"
-    "Samsung Galaxy Z Flip6 AI Smartphone, 6.7 FHD+ AMOLED 2X Display, Octa-Core CPU, 12GB RAM, 512GB Storage, 50MP Camera, Dual-SIM - 5G Network",
+    "OnePlus 12 5G Dual Sim Mobile Phone, 6.8 QHD+ ProXDR 120Hz Display, 12GB RAM, 256GB Storage, Snapdragon 8 Gen 3 Chipset, 50mp+64mp+48mp Main Camera, 32mp Selfie Camera, Silky Black | OP1212R256S",
+    "Samsung Galaxy A34 Dual SIM Smartphone, 6.6 Super AMOLED Display, 6GB RAM, 128GB Storage, LTE 4G / 5G, 48 MP /13 MP Camera, 5000mAh Battery, UAE Version, Awesome Lime",
+    "Xiaomi 14 Ultra Dual SIM 5G 512GB 16GB 5G White - Chines Version",
+    "Samsung Galaxy A15 5G Dual SIM, 4GB RAM, 128GB Storage, Blue Black (UAE Version)",
+    "Apple iPhone 15 Pro Max",
+    "HUAWEI nova 10 Dual Sim Smartphone, 6.67 Curved OLED Display, 8GB RAM 256 GB Storage, LTE 4G, 60 MP Front Ultra Wide Camera",
+    "HUAWEI nova 11i SmartPhone",
+    "vivo V30 5G (Noble Black, 12GB+12GB RAM, 512GB)",
+    "Samsung Galaxy Z Flip6 AI Smartphone",
     "HUAWEI nova 10 Pro Dual SIM Smartphone, 6.7 120 Hz Curved Display, Star Orbit Ring Design, 8GB RAM, 256GB Storage, 4G LTE Network, 60 MP/50MP Camera, 4500 mAh Battery, Starry Black | 51097EWH"
 ]
 
@@ -30,7 +29,7 @@ def global_microless(phone_name):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser') 
     product_link_tag = soup.find('div', class_='product-title').find('a') 
-
+    
     if product_link_tag:
         product_link = product_link_tag['href'] 
         response2 = requests.get(product_link)
@@ -49,6 +48,6 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
             price = 'Error'
             print(f'{phone_name} generated an exception: {exc}')
         phone_info[phone_name] = {'Price': price}
-    
-    
-print(phone_info)   
+        df.loc[df['Phone names'] == phone_name, 'Price'] = price    
+print(df)  
+ 
