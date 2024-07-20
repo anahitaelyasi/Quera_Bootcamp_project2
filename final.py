901eafbb4f42445d3f058ac7983d6e282a925df6
SONRA <166879455+Sonra0@users.noreply.github.com>
2024-07-20 04:43:58 -0600

    Update final.py

--- final.py
+++ final.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
+
+# Step 1 --> Create Dataset
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------------
 commom_phones = ["Motorola Moto G Play 2023 3GB/32GB 16MP Camera - Navy Blue",
                 "Samsung Galaxy S24 Ultra STANDARD EDITION DUAL SIM 512GB ROM + 12GB RAM (GSM | CDMA) Factory Unlocked 5G Smartphone (Titanium Black)",
                 "OnePlus 12 12GB RAM+256GB Dual-SIM Unlocked Android Smartphone Supports Fastest 50W Wireless Charging",
@@ -9,6 +13,7 @@ commom_phones = ["Motorola Moto G Play 2023 3GB/32GB 16MP Camera - Navy Blue",
                 "Apple iPhone 14 Pro Max 256GB Fully Unlocked Purple - Grade B",
                 "Samsung Galaxy A55"
 ]
+
 only_newegg = [
     "iPhone 13 Pro Max 5G 128GB", 
     "SAMSUNG Galaxy S24 Ultra Cell Phone 256GB AI Smartphone Unlocked Android 200MP 100x Zoom Cameras",
@@ -26,12 +31,17 @@ only_microless = [
     "Samsung Galaxy Z Flip6 AI Smartphone",
     "HUAWEI nova 10 Pro Dual SIM Smartphone, 6.7 120 Hz Curved Display, Star Orbit Ring Design, 8GB RAM, 256GB Storage, 4G LTE Network, 60 MP/50MP Camera, 4500 mAh Battery, Starry Black | 51097EWH"
 ]
+
 uncommon_phones = ["Original LG Smart Folder 4G LTE Mobile Phone Unlocked LG X100 3.3'' 2GB RAM 16GB ROM 4.9MP Camera FM Radio Android SmartPhone",
                "HTC Status ChaCha A810a Unlocked Phone with QWERTY Keyboard, 5MP Camera, Wi-Fi and GPS - US Warranty - Silver",
                "Sony Ericsson Xperia X10 Mini E10i",
                "ASUS Phone ZA550KL-S425-1G16G-BK Zenfone Live L1 5.45 1GB 16GB Black Retail"
 ]
+
 df1 = pd.DataFrame(commom_phones,columns='Common phones')
 df2 = pd.DataFrame(only_newegg,columns='Only Newegg')
 df3 = pd.DataFrame(only_microless,columns='Only Microless') 
 df4 = pd.DataFrame(uncommon_phones,columns='Uncommon_phones') 
+
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------------
